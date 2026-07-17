d91c577dd4773f7eddbaf9573c9c40723ee74a18
lauraaromano <lauraromano2018@gmail.com>
2026-07-17 13:16:08 -0300

    first commit

--- tests/e2e/logout.spec.ts
+++ tests/e2e/logout.spec.ts
@@ -1,4 +1,4 @@
-import { test, expect } from '@playwright/test';
+import { test } from '@playwright/test';
 import { Login } from '../support/actions/Login';
 import { admin } from '../support/fixtures/users.json';
 
